0e431667c58379474db6fefd3a94c1c8831465eb
filipvvv <f@filip.world>
2023-02-24 18:44:09 +0100

    Improvements to juice-doc script

--- scripts/juice-doc.sh
+++ scripts/juice-doc.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 # Documents a Juicebox protocol extension using `forge doc`.
-# Update JUICE_DOCS_DIR before running. Make sure src is set to your contracts dir in `foundry.toml`.
+# Update JUICE_DOCS_DIR before running.
 # Run from root directory of extension which you wish to document.
 
 JUICE_DOCS_DIR="/home/filipv/Workspace/juice-docs/"
@@ -24,20 +24,23 @@ rm -rf SUMMARY.md contracts/forge-test contracts/scripts
 # Add frontmatter to README
 echo -e "---\ntitle: $CURRENT_DIR\n---\n$(cat README.md)" > README.md
 
-# Move enums, interfaces, libraries, and structs out from the contracts directory, update links, and create Docusaurus category files.
+# Delete auto-generated README files from forge doc
+find -type f -name README.md ! -path './README.md' -delete
+
+# Move enums, interfaces, libraries, and structs out from the contracts directory, update links, and create Docusaurus category files and READMEs for each.
 for DIR in "enums" "interfaces" "libraries" "structs"
 do
   mv contracts/$DIR .
   echo -e "{\n  \"label\": \"${DIR^}\"\n}" > $DIR/_category_.json
+	echo -e "---\ntitle: ${DIR^}\n---\n\nimport DocCardList from '@theme/DocCardList';\n\n<DocCardList/>" > $DIR/README.md
   grep -rl "contracts/$DIR" | xargs sed -i "s/](\/dev\/api\/extensions\/$CURRENT_DIR\/contracts\/$DIR/](\/dev\/api\/extensions\/$CURRENT_DIR\/$DIR/g"
 done
 
-# Create Docusaurus category files for contracts and abstract.
+# Create Docusaurus category files and READMEs for contracts and abstract.
 echo -e "{\n  \"label\": \"Contracts\"\n}" > contracts/_category_.json
+echo -e "---\ntitle: Contracts\n---\n\nimport DocCardList from '@theme/DocCardList';\n\n<DocCardList/>" > contracts/README.md
 echo -e "{\n  \"label\": \"Abstract\",\n  \"position\": 1\n}" > contracts/abstract/_category_.json
-
-# Replace auto-generated forge README files with Docusaurus Card Lists.
-find -type f -name README.md ! -path './README.md' -exec sed -i '1!d;1c import DocCardList from '@theme/DocCardList';\n<DocCardList/>' {} \;
+echo -e "---\ntitle: Abstract\n---\n\nimport DocCardList from '@theme/DocCardList';\n\n<DocCardList/>" > contracts/abstract/README.md
 
 # Move documents out of .sol directories and update links
 for SOL in $(find ~+ -name *.sol -type d)
@@ -58,3 +61,8 @@ do
 	done
 	grep -rl "/$PREFIX\..*\." | xargs sed -i "s/\/$PREFIX\.\([^\.]*\)/\/\1/g"
 done
+
+# Add backticks to inheritance
+find -type f -name "*.md" -exec sed -i "/\*\*Inherits:\*\*/{ n; s/\[\(\w*\)\]/[\`\1\`]/g ; s/^\([A-Za-z0-9]*\)$/\`\1\`/g }" {} +;
+
+# find -type f -name "*.md" -exec sed -n "/\*\*Inherits:\*\*/{ n ; s/, \([A-Za-z0-9]*\),/\, \`\1\`,/gp }" {} +;
